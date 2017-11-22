5cdf79b92d4645f568287414c0e572d0e607761a
ZP4RKER <zp4rker@programmer.net>
2017-11-22 20:07:45 +1100

    Redesigning serverinfo command #2

--- src/main/java/me/zp4rker/discord/ryno/commands/ServerInfo.java
+++ src/main/java/me/zp4rker/discord/ryno/commands/ServerInfo.java
@@ -36,8 +36,8 @@ public class ServerInfo {
         MessageEmbed embed = new EmbedBuilder()
                 .setAuthor(name).setThumbnail(guild.getIconUrl()).setColor(Ryno.embedColour)
                 .addField("Owner", owner, true).addField("Region", region, true)
-                .addField("Members", "Users: " + users + "\nBots: " + bots, true)
-                .addField("Channels", "**Categories:** " + categories + "\n**Text:** " + textChannels + "\n**Voice:** " + voiceChannels, true)
+                .addField("Members", users + " user(s) and " + bots + " bot(s).", true)
+                .addField("Channels", categories + " categories, " + textChannels + " text channel(s) and " + voiceChannels + " voice channel(s).", true)
                 .addField("Roles", roles + "", true).addField("Emotes", emotes + "", true)
                 .setFooter("ID: " + id, null).build();
 
