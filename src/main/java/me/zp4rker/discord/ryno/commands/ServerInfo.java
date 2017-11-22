0a7c9fd4314f6f74637ff03059e8ae723ec56b7f
ZP4RKER <zp4rker@programmer.net>
2017-11-22 21:08:49 +1100

    Updated serverinfo command

--- src/main/java/me/zp4rker/discord/ryno/commands/ServerInfo.java
+++ src/main/java/me/zp4rker/discord/ryno/commands/ServerInfo.java
@@ -36,8 +36,8 @@ public class ServerInfo {
         MessageEmbed embed = new EmbedBuilder()
                 .setAuthor(name).setThumbnail(guild.getIconUrl()).setColor(Ryno.embedColour)
                 .addField("Owner", owner, true).addField("Region", region, true)
-                .addField("Members", "-> " + users + " user(s)\n-> " + bots + " bot(s)", true)
-                .addField("Channels", "-> " + categories + " categories\n-> " + textChannels + " text channel(s)\n-> " + voiceChannels + " voice channel(s)", true)
+                .addField("Members", "\u2022 " + users + " user(s)\n\u2022 " + bots + " bot(s)", true)
+                .addField("Channels", "\u2022 " + categories + " categories\n\u2022 " + textChannels + " text channel(s)\n\u2022 " + voiceChannels + " voice channel(s)", true)
                 .addField("Roles", roles + "", true).addField("Emotes", emotes + "", true)
                 .setFooter("ID: " + id, null).build();
 
