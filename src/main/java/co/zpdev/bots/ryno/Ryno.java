b7a2a4d420bb98dadfc9e2d2717e9bb482fe1720
zpdev <acc@zpdev.co>
2018-06-20 19:44:55 +1000

    Starting rewrite

---
+++ src/main/java/co/zpdev/bots/ryno/Ryno.java
@@ -0,0 +1,12 @@
+package co.zpdev.bots.ryno;
+
+/**
+ * @author ZP4RKER
+ */
+public class Ryno {
+
+    public static void main(String[] args) {
+        //
+    }
+
+}
--- src/main/java/me/zp4rker/discord/core/util/PBTask.java
+++
@@ -1,5 +0,0 @@
-package me.zp4rker.discord.core.util;
-
-public abstract class PBTask {
-    public abstract void handle(String s);
-}
